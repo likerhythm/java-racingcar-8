ca07e1105b2f4bc892c9c352adf20b10c2fa9f1a
likerhythm <wjdalsdl0412@gmail.com>
2025-10-22 13:21:11 +0900

    test(car): 자동차 테스트
    테스트 목록
    - 자동차 이름 글자 수 및 공백 유효성 테스트
    - 자동차 후진 불가능 테스트

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CarTest {
+
+    @Test
+    void 자동차_이름은_5글자_이하만_가능합니다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Car("여섯글자이름"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" 공백시작", "공백끝 ", " 시작끝 "})
+    void 자동차_이름은_공백으로_시작하거나_끝날_수_없습니다(String value) {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Car(value));
+    }
+
+    @Test
+    void 자동차는_후진할_수_없습니다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new Car("이름").move(-1));
+    }
+}
