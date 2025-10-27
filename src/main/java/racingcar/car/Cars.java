9a53da8e6bad83cfe2c9c84e0bb2618d1ca53307
likerhythm <wjdalsdl0412@gmail.com>
2025-10-27 12:52:34 +0900

    feat(cars): 자동차 이름 중복 유효성 검사 구현

--- src/main/java/racingcar/car/Cars.java
+++ src/main/java/racingcar/car/Cars.java
@@ -8,10 +8,16 @@ public class Cars {
     private List<Car> cars;
 
     public Cars(List<Car> cars) {
+        validateCarNames(cars);
         this.cars = cars;
     }
 
     public List<MoveResult> move() {
         return cars.stream().map(Car::move).toList();
+    }
+
+    private void validateCarNames(List<Car> cars) {
+        long distinctNameCount = cars.stream().map(Car::getName).distinct().count();
+        if (distinctNameCount != cars.size()) throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다");
     }
 }
