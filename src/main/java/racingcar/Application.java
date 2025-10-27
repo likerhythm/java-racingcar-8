aaaf52f6ea50a183d2abe46fadb227cfd65fa7be
likerhythm <wjdalsdl0412@gmail.com>
2025-10-27 12:54:08 +0900

    refactor: 게임 생성 시 랜덤 정수 생성자 설정하도록 수정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,10 @@
 package racingcar;
 
+import racingcar.number_generator.DefaultRandomIntegerGenerator;
+
 public class Application {
     public static void main(String[] args) {
-        GameConfigurator configurator = new GameConfigurator();
+        GameConfigurator configurator = new GameConfigurator(new DefaultRandomIntegerGenerator());
         RacingGame racingGame = configurator.createRacingGame();
         racingGame.run();
     }
--- src/main/java/racingcar/GameConfigurator.java
+++ src/main/java/racingcar/GameConfigurator.java
@@ -1,29 +1,31 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
 import java.util.List;
 import racingcar.car.Car;
 import racingcar.car.Cars;
-import racingcar.number_generator.DefaultRandomIntegerGenerator;
 import racingcar.number_generator.RandomIntegerGenerator;
 import racingcar.view.InputView;
 
 public class GameConfigurator {
 
     private InputView inputView;
+    private RandomIntegerGenerator generator;
 
-    public GameConfigurator() {
+    public GameConfigurator(RandomIntegerGenerator generator) {
         this.inputView = new InputView();
+        this.generator = generator;
     }
 
     public RacingGame createRacingGame() {
         String carNames = inputView.printCarNameInputGuide();
-        RandomIntegerGenerator generator = new DefaultRandomIntegerGenerator();
         Driver driver = new Driver(generator);
         List<Car> cars = Arrays.stream(carNames.split(",")).map(cn -> new Car(cn, driver)).toList();
         try {
             int gameStage = Integer.parseInt(inputView.printGameStageInputGuide());
             GameController gameController = new GameController(gameStage, new Cars(cars));
+            Console.close();
             return new RacingGame(gameController);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("시도 횟수는 정수로 입력해야 합니다");
